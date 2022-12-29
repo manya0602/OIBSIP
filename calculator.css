5a5cb6b326117ad948909ee2d19ed260826c39c3
manya0602 <97423136+manya0602@users.noreply.github.com>
2022-12-29 23:46:38 +0530

    Create calculator.css

---
+++ calculator.css
@@ -0,0 +1,47 @@
+.cal {
+    max-width: 400px;
+    height: 435px;
+    margin: 10vh auto 0 auto;
+    
+  }
+  body {
+    height: 85vh;
+    background-color: rgb(233, 217, 226);
+  }
+  h1 {
+    margin-top: 3%;
+    text-align: center;
+    font-size: 40px;
+  }
+  .results{
+    text-align: left;
+    height: 60px;
+    line-height: 60px;
+    padding: 20px 20px;
+    font-size: 25px;
+    border: 3px solid rgb(162, 13, 45);        ;
+    color: black;
+    font-weight: bold;
+  }
+  .buttons {
+    grid-template-columns: 0.5fr 0.5fr 0.5fr 0.5fr;
+    display: grid;
+    border-bottom: 3px solid rgb(162, 13, 45);
+    border-left: 3px solid rgb(162, 13, 45);
+    border-right: 3px solid rgb(162, 13, 45);
+  }
+  .btn {
+    border: 2.5px solid rgb(162, 13, 45);
+    height: 60px;
+    line-height: 60px;
+    text-align: center;
+    margin: 0.3%;
+    font-size: 26px;
+    cursor: pointer;
+    font-weight: 900;
+    border-radius: 7%;
+  }
+  .btn:hover {
+    background: #9d8e8e ;
+    color: white;
+  }
